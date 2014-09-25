49ec32fdf7fed1611df95da067d424515bcfaaa5
Kevin Burke <kev@inburke.com>
2014-09-24 21:13:20 -0700

    Use OVRCameraController instead of OVRPlayerController

--- Assets/TargetScript.cs
+++ Assets/TargetScript.cs
@@ -24,6 +24,7 @@ public class TargetScript : MonoBehaviour {
 			target.transform.position = new Vector3(Random.Range (-10.0f, 10.0f), 0.64f, Random.Range (-10.0f, 10.0f));
 			Vector3 diff = target.transform.position - transform.position;
 			target.transform.rotation = Quaternion.LookRotation(diff);
+			Debug.Log (Quaternion.LookRotation(diff));
 			target.SetActive(true);
 			Debug.Log(target.GetInstanceID());
 			objectsVisible++;
