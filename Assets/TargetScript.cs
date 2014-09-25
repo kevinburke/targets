73feeca71c9330267ce41ff4c6fa87c16c90b7f0
Kevin Burke <kev@inburke.com>
2014-09-25 13:52:16 -0700

    Determine when the camera is looking at the square.

--- Assets/TargetScript.cs
+++ Assets/TargetScript.cs
@@ -11,25 +11,37 @@ public class TargetScript : MonoBehaviour {
 	void Start () {
 		count = 0;
 		objectsVisible = 0;
-		Debug.Log ("Hello from the Start script.");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	    if (count < 1) {
-			Debug.Log("The first frame.");
-		}
 		count++;
 		if (objectsVisible == 0) {
 			target = GameObject.CreatePrimitive(PrimitiveType.Quad);
 			target.renderer.material.color = new Color(217.0f/256.0f, 79.0f/256.0f, 83.0f/256.0f);
 			target.transform.position = new Vector3(Random.Range (-2.0f, 2.0f), Random.Range (-0.5f, 2.0f), Random.Range (3.0f, 5.0f));
 			Debug.Log (Camera.main.transform.position);
-			target.transform.rotation =  Quaternion.LookRotation(target.transform.position - Camera.main.transform.position);
+
+			// orient the quad so it's facing at the user
+			target.transform.rotation = Quaternion.LookRotation(target.transform.position - Camera.main.transform.position);
+
+			// make it visible
 			target.SetActive(true);
-			Debug.Log (Camera.main.transform.forward);
-			Debug.Log(target.GetInstanceID());
 			objectsVisible++;
+		}
+		// check if user is looking at the object
+		float length = 10.0f;
+		RaycastHit hit;
+		Vector3 rayDirection = Camera.main.transform.TransformDirection (Vector3.forward);
+		Vector3 rayStart = Camera.main.transform.position + rayDirection;
+		Debug.DrawRay (rayStart, rayDirection * length, Color.green);
+		if (Physics.Raycast (rayStart, rayDirection, out hit, length)) {
+			Debug.Log (hit);
+			Debug.Log ("a hit!");
+		} else {
+			if (count % 50 == 0) {
+				Debug.Log ("no hit");
+			}
 		}
 		if (count % 50 == 0) {
 			Debug.Log (Camera.main.transform.rotation);
