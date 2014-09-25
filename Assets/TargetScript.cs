16d694452fc0d70f604f937b49f8c1f6363c0db5
Kevin Burke <kev@inburke.com>
2014-09-24 23:06:12 -0700

    the quad is red

--- Assets/TargetScript.cs
+++ Assets/TargetScript.cs
@@ -22,6 +22,7 @@ public class TargetScript : MonoBehaviour {
 		count++;
 		if (objectsVisible == 0) {
 			target = GameObject.CreatePrimitive(PrimitiveType.Quad);
+			target.renderer.material.color = new Color(217.0f/256.0f, 79.0f/256.0f, 83.0f/256.0f);
 			target.transform.position = new Vector3(Random.Range (-2.0f, 2.0f), Random.Range (-0.5f, 2.0f), Random.Range (3.0f, 5.0f));
 			Debug.Log (Camera.main.transform.position);
 			target.transform.rotation =  Quaternion.LookRotation(target.transform.position - Camera.main.transform.position);
@@ -29,6 +30,9 @@ public class TargetScript : MonoBehaviour {
 			Debug.Log (Camera.main.transform.forward);
 			Debug.Log(target.GetInstanceID());
 			objectsVisible++;
+		}
+		if (count % 50 == 0) {
+			Debug.Log (Camera.main.transform.rotation);
 		}
 	}
 }
